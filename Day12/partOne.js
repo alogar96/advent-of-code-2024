bd63526e5d74d6d7da4942f189f733a3ed918770
Andraz <andraz@logar.dev>
2024-12-12 20:19:08 +0100

    Day 12 code cleanup

--- Day12/partOne.js
+++ Day12/partOne.js
@@ -5,58 +5,50 @@ const fs = require('node:fs');
 const data = fs.readFileSync('input.txt', 'utf-8').trim().split('\n').map(x => x.trim());
 
 let result = 0;
+let directions = [[0,1],[1,0],[-1,0],[0,-1]];
+
+let id = 0;
 let areas = {};
 let seen = {};
-
-let directions = [[0,1],[1,0],[-1,0],[0,-1]];
-let id = 0;
-
-let areaCounters = [];
 
 // Check if array is already in main array
 function arrInArray(array, arr) {
     return array.some(element => JSON.stringify(element) === JSON.stringify(arr));
 }
 
-
+// Find all areas
 for (let r = 0; r < data.length; r++) {
     for (let c = 0; c < data[r].length; c++) {
-        const seenKey = JSON.stringify([r,c]);
-        if (seen[seenKey] === undefined) {
-            let areaCount = 0;
-            let areaId = data[r][c];
+        let perimeter  = 0;
+        if (seen[JSON.stringify([r,c])] === undefined) {
+            areas[id] = [[r,c]];
             let currentAreas = [[r,c]];
-            areas[id] = [[r, c]];
             while (currentAreas.length > 0) {
-                let currentA = currentAreas.pop();
+                let cA = currentAreas.pop();
                 for (let i = 0; i < directions.length; i++) {
-                    let cY = currentA[0] + directions[i][0];
-                    let cX = currentA[1] + directions[i][1];
+                    let cY = cA[0] + directions[i][0];
+                    let cX = cA[1] + directions[i][1];
                     if (cY >= 0 && cY < data.length && cX >= 0 && cX < data[0].length) {
-                        if (areaId == data[cY][cX] &! arrInArray(areas[id], [cY, cX])) {
-                            const test = JSON.stringify([cY,cX]);
-                            seen[test] = true;
+                        if (data[r][c] == data[cY][cX] &! arrInArray(areas[id], [cY, cX])) {
+                            seen[JSON.stringify([cY,cX])] = true;
                             currentAreas.push([cY, cX]);
                             areas[id].push([cY, cX]);
-                        } else if (areaId != data[cY][cX]) {
-                            areaCount++;
+                        } else if (data[r][c] != data[cY][cX]) {
+                            perimeter++;
                         }
                     } else {
-                        areaCount++;
+                        perimeter++;
                     }
                 }
             }
-            areaCounters.push(areaCount)
-            id = id +1;
+            // Calculate area size * perimeter
+            //console.log(areas)
+            result = result + (perimeter * areas[id].length);
+            //console.log(areas)
+            id++;
         }
     }
 }
 
-for (let k = 0; k < areaCounters.length; k++) {
-    result = result + (areaCounters[k] * areas[k].length)
-}
-
-console.log(areas)
-console.log(areaCounters)
-
+// Get result 
 console.log(result);
