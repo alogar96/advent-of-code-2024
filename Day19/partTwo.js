8b2d19662ce81800f35d25f7bb10debac97de141
Andraz <andraz@logar.dev>
2024-12-21 18:50:01 +0100

    Day 19 solution part 2

--- Day19/partTwo.js
+++ Day19/partTwo.js
@@ -2,39 +2,46 @@
 // Puzzle: https://adventofcode.com/2024/day/19
 
 const fs = require('node:fs');
-const data = fs.readFileSync('test.txt', 'utf-8').split('\r\n');
+const data = fs.readFileSync('input.txt', 'utf-8').split('\n');
 let combinations = data[0].split(',').map(x => x.trim());
 
 let result = 0;
 
-// Check if design is valid
-function validDesign(match) {
-    let solutions = [];
-    let parts = [match.substring(0,1)];
-    while (parts.length > 0 && match != '') {
-        let str = parts.pop();
-
-        let start = str.length;
-        for (let i = start; i < match.length; i++) {
-            let part = match.substring(start, i+1);
-            if (combinations.includes(part)) {
-                let newStr = str + part;
-                if (newStr == match)
-                    solutions.push(newStr)
-                else
-                    parts.push(newStr)
-            }
-        }
+function countDesigns(match) {
+    let search = []
+    let parts = [''];
+    // Split match to substrings parts
+    for (let i = 0; i < match.length-1; i++) {
+        parts.push(match.substring(0, i+1));
     }
 
-    return solutions.length;
+    // Look for number of solutions for each math part
+    for (let p = parts.length-1; p >= 0; p--) {
+        let solutions = 0;
+        for (let c = 0; c < combinations.length; c++) {
+            let str = parts[p] + combinations[c];
+            if (str == match)
+                solutions++;
+            else {
+                for (let s = 0; s < search.length; s++) {
+                    if (str == search[s][0]) 
+                        solutions = solutions + search[s][1];
+                }
+            }
+        }
+        // Remember number of solutions for each part
+        search.push([parts[p], solutions]);
+    }
+
+    // Return total count of solutions 
+    let count = search[search.length-1][1];
+    return count;
 }
 
 // Check which patterns 
 for (let p = 2; p < data.length; p++) {
-    console.log(p)
-    result = result + validDesign(data[p]);
+    result = result + countDesigns(data[p]);
 }
 
 // Show the result
-console.log(result)
+console.log(result);
